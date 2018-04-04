5df8d7b1240b0a7acb1e2acc8b7b76a53158336d
vincent <likonion@outlook.com>
2018-04-04 10:57:24 +0800

    update vux

--- src/components/index.vue
+++ src/components/index.vue
@@ -34,10 +34,10 @@
         <div style="padding:10px; background: none">
           <flexbox>
             <flexbox-item>
-              <x-button type="primary" @click.native="">搜索</x-button>
+              <x-button type="primary" >搜索</x-button>
             </flexbox-item>
             <flexbox-item>
-              <x-button type="default" @click.native="">重置</x-button>
+              <x-button type="default" >重置</x-button>
             </flexbox-item>
             <flexbox-item>
               <x-button ref="copy" class="copy" type="default" :data-clipboard-text="copyText" @click.native="copyToClipboard">📋</x-button>
@@ -101,352 +101,380 @@
 </template>
 
 <script>
-  import Clipboard from 'clipboard';  
-  import { mapState, mapGetters, mapMutations } from 'vuex'
-  import {
-    XHeader, Tabbar, TabbarItem, Card, TransferDom, Group, Cell, XButton, Drawer, ViewBox, Radio, Loading, Calendar,
-    Selector, PopupPicker, Picker, XInput, Flexbox, FlexboxItem, Popup
-  } from 'vux'
+import Clipboard from "clipboard";
+import { mapState, mapGetters, mapMutations } from "vuex";
+import {
+  XHeader,
+  Tabbar,
+  TabbarItem,
+  Card,
+  TransferDom,
+  Group,
+  Cell,
+  XButton,
+  Drawer,
+  ViewBox,
+  Radio,
+  Loading,
+  Calendar,
+  Selector,
+  PopupPicker,
+  Picker,
+  XInput,
+  Flexbox,
+  FlexboxItem,
+  Popup
+} from "vux";
 
-  export default {
-    components: {
-      XHeader,
-      Tabbar,
-      TabbarItem,
-      Card,
-      TransferDom,
-      Group,
-      Cell,
-      XButton,
-      Drawer,
-      ViewBox,
-      Radio,
-      Loading,
-      Calendar,
-      Selector,
-      PopupPicker,
-      Picker,
-      XInput,
-      Flexbox,
-      FlexboxItem,
-      Popup
-    },
-    directives: {
-      TransferDom
-    },
-    watch: {
-    },
-    mounted () {
-      // 添加滚动监听事件
-      this.$nextTick(function () {
-        if (this.$route.path == '/orderList' || this.$route.path == '/waitOrderList') {
-          setTimeout(() => {
-            this.box = document.querySelector('#vux_view_box_body')
-            if (this.box) {
-              this.box.removeEventListener('scroll', this.handler, false)
-              this.box.addEventListener('scroll', this.handler, false)
-            }
-          }, 1000)
-        }
-      })
-      // 滚动事件
-      this.handler = () => {
-        if (this.$route.path === '/orderList' || this.$route.path == '/waitOrderList') {
-          this.box = document.querySelector('#vux_view_box_body')
-          let scrollTop = this.box.scrollTop
-          this.$store.commit('updatePagePosition', {top: scrollTop})
-        }
+export default {
+  components: {
+    XHeader,
+    Tabbar,
+    TabbarItem,
+    Card,
+    TransferDom,
+    Group,
+    Cell,
+    XButton,
+    Drawer,
+    ViewBox,
+    Radio,
+    Loading,
+    Calendar,
+    Selector,
+    PopupPicker,
+    Picker,
+    XInput,
+    Flexbox,
+    FlexboxItem,
+    Popup
+  },
+  directives: {
+    TransferDom
+  },
+  watch: {},
+  mounted() {
+    // 添加滚动监听事件
+    this.$nextTick(function() {
+      if (
+        this.$route.path == "/orderList" ||
+        this.$route.path == "/waitOrderList"
+      ) {
+        setTimeout(() => {
+          this.box = document.querySelector("#vux_view_box_body");
+          if (this.box) {
+            this.box.removeEventListener("scroll", this.handler, false);
+            this.box.addEventListener("scroll", this.handler, false);
+          }
+        }, 1000);
       }
-        this.copyBtn = new Clipboard(".copy");
+    });
+    // 滚动事件
+    this.handler = () => {
+      if (
+        this.$route.path === "/orderList" ||
+        this.$route.path == "/waitOrderList"
+      ) {
+        this.box = document.querySelector("#vux_view_box_body");
+        let scrollTop = this.box.scrollTop;
+        this.$store.commit("updatePagePosition", { top: scrollTop });
+      }
+    };
+    this.copyBtn = new Clipboard(".copy");
+  },
+  data() {
+    return {
+      show: true,
+      drawerVisibility: false,
+      showMode: "overlay",
+      showModeValue: "overlay",
+      showPlacement: "right",
+      showPlacementValue: "right",
+      starDate: "2017-07-13",
+      endDate: "2017-07-13",
+      keyword: "",
+      selectState: ["请选择"],
+      orderState: [["未处理", "处理中", "已处理"]],
+      show7: true,
+      path: this.$route.path,
+      copyText: "我是可以复制的内容，啦啦啦啦",
+      copyBtn: null //存储初始化复制按钮事件
+    };
+  },
+  computed: {
+    ...mapState({
+      orders: "orders",
+      clickOrderIndex: "clickOrderIndex"
+    }),
+    ...mapGetters({
+      waitOrders: "waitOrders",
+      waitOrdersNum: "waitOrdersNum",
+      currentOrder: "currentOrder"
+    }),
+    headTitle() {
+      if (this.$route.params.id) {
+        return this.currentOrder.number;
+      } else {
+        return this.$route.name;
+      }
     },
-    data () {
+    viewOrderUrl() {
+      if (this.clickOrderIndex !== "") {
+        return "/orderview/" + this.currentOrder.id;
+      }
+    },
+    ManOrderUrl() {
+      if (this.clickOrderIndex !== "") {
+        return "/orderManage/" + this.currentOrder.id;
+      }
+    },
+    leftOptions() {
       return {
-        show: true,
-        drawerVisibility: false,
-        showMode: 'overlay',
-        showModeValue: 'overlay',
-        showPlacement: 'right',
-        showPlacementValue: 'right',
-        starDate: '2017-07-13',
-        endDate: '2017-07-13',
-        keyword: '',
-        selectState: ['请选择'],
-        orderState: [['未处理', '处理中', '已处理']],
-        show7: true,
-        path: this.$route.path,
-        copyText: "我是可以复制的内容，啦啦啦啦",
-        copyBtn: null //存储初始化复制按钮事件
-      }
+        showBack: this.$route.path !== "/"
+      };
     },
-    computed: {
-      ...mapState({
-        orders: 'orders',
-        clickOrderIndex: 'clickOrderIndex'
-      }),
-      ...mapGetters({
-        waitOrders: 'waitOrders',
-        waitOrdersNum: 'waitOrdersNum',
-        currentOrder: 'currentOrder'
-      }),
-      headTitle () {
-        if (this.$route.params.id) {
-          return this.currentOrder.number
-        } else {
-          return this.$route.name
-        }
-      },
-      viewOrderUrl () {
-        if (this.clickOrderIndex !== '') {
-          return '/orderview/' + this.currentOrder.id
-        }
-      },
-      ManOrderUrl () {
-        if (this.clickOrderIndex !== '') {
-          return '/orderManage/' + this.currentOrder.id
-        }
-      },
-      leftOptions () {
-        return {
-          showBack: this.$route.path !== '/'
-        }
-      },
-      rightOptions () {
-        return {
-          showMore: false
-        }
-      }
+    rightOptions() {
+      return {
+        showMore: false
+      };
+    }
+  },
+  created() {
+    this.$store.dispatch("getData");
+  },
+  methods: {
+    clickTab() {
+      console.log("in");
     },
-    created () {
-      this.$store.dispatch('getData')
+    onClickMore() {
+      this.showMenu = true;
     },
-    methods: {
-      clickTab () {
-        console.log('in')
-      },
-      onClickMore () {
-        this.showMenu = true
-      },
-      onShowModeChange (val) {
-        /** hide drawer before changing showMode **/
-        this.drawerVisibility = false
-        setTimeout(one => {
-          this.showModeValue = val
-        }, 400)
-      },
-      onPlacementChange (val) {
-        /** hide drawer before changing position **/
-        this.drawerVisibility = false
-        setTimeout(one => {
-          this.showPlacementValue = val
-        }, 400)
-      },
-      // 复制内容到剪贴板
-      copyToClipboard () {
-        let _this = this;
-        let clipboard = _this.copyBtn == null ? new Clipboard(".copy") : _this.copyBtn
-        clipboard.on('success', function() {
-            console.log("复制成功！")
-            _this.copyBtn = null
-            clipboard.destroy();
-        });
-        clipboard.on('error', function() {
-            console.log("复制失败，请手动选择复制！")
-            _this.copyBtn = null
-            clipboard.destroy();
-        });
-      }
+    onShowModeChange(val) {
+      /** hide drawer before changing showMode **/
+      this.drawerVisibility = false;
+      setTimeout(one => {
+        this.showModeValue = val;
+      }, 400);
+    },
+    onPlacementChange(val) {
+      /** hide drawer before changing position **/
+      this.drawerVisibility = false;
+      setTimeout(one => {
+        this.showPlacementValue = val;
+      }, 400);
+    },
+    // 复制内容到剪贴板
+    copyToClipboard() {
+      let _this = this;
+      let clipboard =
+        _this.copyBtn == null ? new Clipboard(".copy") : _this.copyBtn;
+      clipboard.on("success", function() {
+        console.log("复制成功！");
+        _this.copyBtn = null;
+        clipboard.destroy();
+      });
+      clipboard.on("error", function() {
+        console.log("复制失败，请手动选择复制！");
+        _this.copyBtn = null;
+        clipboard.destroy();
+      });
     }
   }
+};
 </script>
 
 <style lang="less">
-  @import '~vux/src/styles/reset.less';
-  @import '~vux/src/styles/1px.less';
-  @import '~vux/src/styles/tap.less';
+@import "~vux/src/styles/reset.less";
+@import "~vux/src/styles/1px.less";
+@import "~vux/src/styles/tap.less";
 
-  body {
-    background-color: #fbf9fe;
+body {
+  background-color: #fbf9fe;
+}
+
+html,
+body {
+  height: 100%;
+  width: 100%;
+  overflow-x: hidden;
+}
+
+.weui-cell {
+  font-size: 14px;
+}
+
+.mainWarpper {
+  height: 100%;
+  width: 100%;
+  overflow-x: hidden;
+}
+
+.m-tablebox {
+  font-size: 0.7rem;
+  background-color: #fff;
+  thead {
+    white-space: nowrap;
+    th {
+      padding: 0.5rem;
+      color: white;
+      font-weight: 700;
+      background-color: #2196f3;
+      > div {
+        height: 1.2rem;
+        line-height: 1.2rem;
+      }
+    }
   }
-
-  html, body {
-    height: 100%;
-    width: 100%;
-    overflow-x: hidden;
-  }
-
-  .weui-cell {
-    font-size: 14px;
-  }
-
-  .mainWarpper {
-    height: 100%;
-    width: 100%;
-    overflow-x: hidden;
-  }
-
-  .m-tablebox {
-    font-size: .7rem;
-    background-color: #fff;
-    thead {
-      white-space: nowrap;
-      th {
+  tbody {
+    padding-top: 100px;
+    background-color: white;
+    tr {
+      &:nth-child(even) {
+        background-color: rgba(239, 239, 244, 0.37);
+      }
+      &.active {
+        background-color: rgba(253, 181, 0, 0.27);
+        a {
+          color: #fff;
+        }
+      }
+      td {
         padding: 0.5rem;
-        color: white;
-        font-weight: 700;
-        background-color: #2196F3;
+        color: #666;
+        &:first-child {
+          color: blue;
+        }
         > div {
-          height: 1.2rem;
-          line-height: 1.2rem;
-        }
-      }
-    }
-    tbody {
-      padding-top: 100px;
-      background-color: white;
-      tr {
-
-        &:nth-child(even) {
-          background-color: rgba(239, 239, 244, 0.37);
-        }
-        &.active {
-          background-color: rgba(253, 181, 0, 0.27);
-          a {
-            color: #fff;
-          }
-        }
-        td {
-          padding: 0.5rem;
-          color: #666;
-          &:first-child {
-            color: blue;
-          }
-          > div {
-            white-space: nowrap;
-          }
+          white-space: nowrap;
         }
       }
     }
   }
+}
 
-  .demo-icon-22 {
-    font-family: 'vux-demo';
-    font-size: 22px;
-    color: #888;
-  }
+.demo-icon-22 {
+  font-family: "vux-demo";
+  font-size: 22px;
+  color: #888;
+}
 
-  .vux-demo-tabbar {
-    .weui-tabbar__item {
+.vux-demo-tabbar {
+  .weui-tabbar__item {
+    .vux-x-icon {
+      fill: #999;
+    }
+    &.weui-bar__item_on {
       .vux-x-icon {
-        fill: #999;
-      }
-      &.weui-bar__item_on {
-        .vux-x-icon {
-          fill: #09BB07;
-        }
+        fill: #09bb07;
       }
     }
   }
+}
 
-  .weui-tabbar.vux-demo-tabbar {
-    /** backdrop-filter: blur(10px);
+.weui-tabbar.vux-demo-tabbar {
+  /** backdrop-filter: blur(10px);
     background-color: none;
     background: rgba(247, 247, 250, 0.5);**/
-  }
+}
 
-  .vux-demo-tabbar .weui-bar__item_on .demo-icon-22 {
-    color: #F70968;
-  }
+.vux-demo-tabbar .weui-bar__item_on .demo-icon-22 {
+  color: #f70968;
+}
 
-  .vux-demo-tabbar .weui-tabbar_item.weui-bar__item_on .vux-demo-tabbar-icon-home {
-    color: rgb(53, 73, 94);
-  }
+.vux-demo-tabbar
+  .weui-tabbar_item.weui-bar__item_on
+  .vux-demo-tabbar-icon-home {
+  color: rgb(53, 73, 94);
+}
 
-  .demo-icon-22:before {
-    content: attr(icon);
-  }
+.demo-icon-22:before {
+  content: attr(icon);
+}
 
-  .vux-demo-tabbar-component {
-    background-color: #F70968;
-    color: #fff;
-    border-radius: 7px;
-    padding: 0 4px;
-    line-height: 14px;
-  }
+.vux-demo-tabbar-component {
+  background-color: #f70968;
+  color: #fff;
+  border-radius: 7px;
+  padding: 0 4px;
+  line-height: 14px;
+}
 
-  .weui-tabbar__icon + .weui-tabbar__label {
-    margin-top: 0 !important;
-  }
+.weui-tabbar__icon + .weui-tabbar__label {
+  margin-top: 0 !important;
+}
 
-  .vux-demo-header-box {
-    z-index: 100;
-    position: absolute;
-    width: 100%;
-    left: 0;
-    top: 0;
-  }
+.vux-demo-header-box {
+  z-index: 100;
+  position: absolute;
+  width: 100%;
+  left: 0;
+  top: 0;
+}
 
-  @font-face {
-    font-family: 'vux-demo';  /* project id 70323 */
-    src: url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.eot');
-    src: url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.eot?#iefix') format('embedded-opentype'),
-    url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.woff') format('woff'),
-    url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.ttf') format('truetype'),
-    url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.svg#iconfont') format('svg');
-  }
+@font-face {
+  font-family: "vux-demo"; /* project id 70323 */
+  src: url("https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.eot");
+  src: url("https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.eot?#iefix")
+      format("embedded-opentype"),
+    url("https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.woff") format("woff"),
+    url("https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.ttf") format("truetype"),
+    url("https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.svg#iconfont")
+      format("svg");
+}
 
-  .demo-icon {
-    font-family: 'vux-demo';
-    font-size: 20px;
-    color: #04BE02;
-  }
+.demo-icon {
+  font-family: "vux-demo";
+  font-size: 20px;
+  color: #04be02;
+}
 
-  .demo-icon-big {
-    font-size: 28px;
-  }
+.demo-icon-big {
+  font-size: 28px;
+}
 
-  .demo-icon:before {
-    content: attr(icon);
-  }
+.demo-icon:before {
+  content: attr(icon);
+}
 
-  .router-view {
-    width: 100%;
-    top: 46px;
-  }
+.router-view {
+  width: 100%;
+  top: 46px;
+}
 
-  .vux-pop-out-enter-active,
-  .vux-pop-out-leave-active,
-  .vux-pop-in-enter-active,
-  .vux-pop-in-leave-active {
-    will-change: transform;
-    transition: all 500ms;
-    height: 100%;
-    top: 46px;
-    position: absolute;
-    backface-visibility: hidden;
-    perspective: 1000;
-  }
+.vux-pop-out-enter-active,
+.vux-pop-out-leave-active,
+.vux-pop-in-enter-active,
+.vux-pop-in-leave-active {
+  will-change: transform;
+  transition: all 500ms;
+  height: 100%;
+  top: 46px;
+  position: absolute;
+  backface-visibility: hidden;
+  perspective: 1000;
+}
 
-  .vux-pop-out-enter {
-    opacity: 0;
-    transform: translate3d(-100%, 0, 0);
-  }
+.vux-pop-out-enter {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
 
-  .vux-pop-out-leave-active {
-    opacity: 0;
-    transform: translate3d(100%, 0, 0);
-  }
+.vux-pop-out-leave-active {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
 
-  .vux-pop-in-enter {
-    opacity: 0;
-    transform: translate3d(100%, 0, 0);
-  }
+.vux-pop-in-enter {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
 
-  .vux-pop-in-leave-active {
-    opacity: 0;
-    transform: translate3d(-100%, 0, 0);
-  }
+.vux-pop-in-leave-active {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
 
-  .menu-title {
-    color: #888;
-  }
+.menu-title {
+  color: #888;
+}
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -9,8 +9,10 @@
     <box gap="10px 10px">
       <flexbox>
         <flexbox-item>
-          <x-button type="primary" @click.native="login('')">登录</x-button>
-          <toast v-model="showPositionValue" type="warn" :time="800" is-show-mask :text="toastTxt" :position="position"></toast>
+          <x-button type="primary" @click.native="login">登录</x-button>
+          <a href="javascript:;" @click="inTo" style="font-size:.5rem;float:right;margin-top:.7rem;color:#999;">直接进入>></a>
+          <x-button type="default" :mini="true" @click.native="login"></x-button>
+          <toast v-model="showPositionValue" type="warn" :time="800" is-show-mask :text="toastTxt" ></toast>
           <div v-transfer-dom>
             <loading v-model="show1" :text="text1"></loading>
           </div>
@@ -21,56 +23,75 @@
 </template>
 
 <script>
-  import { XHeader, Group, Box, XButton, Flexbox, FlexboxItem, XInput, Toast, Loading, TransferDomDirective as TransferDom } from 'vux'
-  export default {
-    data () {
-      return {
-        username: '',
-        password: '',
-        position: 'default',
-        showPositionValue: false,
-        toastTxt: '',
-        show1: false,
-        text1: 'Processing'
+import {
+  XHeader,
+  Group,
+  Box,
+  XButton,
+  Flexbox,
+  FlexboxItem,
+  XInput,
+  Toast,
+  Loading,
+  TransferDomDirective as TransferDom
+} from "vux";
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+      position: "default",
+      showPositionValue: false,
+      toastTxt: "",
+      show1: false,
+      text1: "Processing"
+    };
+  },
+  directives: {
+    TransferDom
+  },
+  created() {},
+  components: {
+    XHeader,
+    Box,
+    Group,
+    XButton,
+    Flexbox,
+    FlexboxItem,
+    XInput,
+    Toast,
+    Loading
+  },
+  methods: {
+    login() {
+      if (!this.username || !this.password) {
+        this.showPositionValue = true;
+        this.toastTxt = "用户名或密码不能为空";
+        return false;
+      }
+      //
+      if (this.username == "123@123.com" && this.password == "123") {
+        this.inTo();
+      } else {
+        this.showPositionValue = true;
+        this.toastTxt = "用户名或密码不正确";
       }
     },
-    directives: {
-      TransferDom
+    inTo() {
+      this.$vux.loading.show({
+        text: "Loading"
+      });
+      setTimeout(() => {
+        this.$router.push({ path: "/home" });
+        this.$vux.loading.hide();
+      }, 300);
     },
-    created () {
-    },
-    components: {
-      XHeader, Box, Group, XButton, Flexbox, FlexboxItem, XInput, Toast, Loading
-    },
-    methods: {
-      login (position) {
-        if (!this.username || !this.password) {
-          this.position = position
-          this.showPositionValue = true
-          this.toastTxt = '用户名或密码不能为空'
-          return false
-        }
-//
-        if (this.username == '123@123.com' && this.password == '123') {
-          this.$vux.loading.show({
-            text: 'Loading'
-          })
-          setTimeout(() => {
-            this.$router.push({path: '/home'})
-            this.$vux.loading.hide()
-          }, 1000)
-        } else {
-          this.showPositionValue = true
-          this.toastTxt = '用户名或密码不正确'
-        }
-      },
-
-      reset () {
-        this.username = ''
-        this.password = ''
-      }
+    reset() {
+      this.username = "";
+      this.password = "";
     }
   }
+};
 </script>
 
 <style></style>
